58ea3893a2743ebafd5a8344cd32d2d4c9dc65da
Jophy Johnson <johnson.jophy27@gmail.com>
2016-10-10 15:39:18 -0500

    about page changed

--- src/about/index.js
+++ src/about/index.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { signInSuccess } from '../data/auth'
 
-const mapStateToProps = (state) => {
-  return {
-       loggedIn: state._root.entries[0][1].loggedIn,
-       userid: state._root.entries[0][1].userid,
-       token: state._root.entries[0][1].token,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    signInSuccess: (loggedIn, userid, token) => {
-      dispatch(signInSuccess(loggedIn, userid, token))
-    }
-  }
-}
-
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: ''
-    }
-  }
-
-  render() {
-    return (
+export default function About() {
+  return (
+    <div>
       <h1>About</h1>
-    )
-  }
+    </div>
+  );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(About);
+About.displayName = 'About';
